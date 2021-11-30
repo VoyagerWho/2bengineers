c4af3b57c1c030fbd92bcf5c05ade479ecb81e9f
Paweł Bielecki <pawelek24@op.pl>
2021-11-30 19:32:29 +0100

    Repairment of compression force direction, added a render engine

--- tbsymulator/mechanics.py
+++ tbsymulator/mechanics.py
@@ -5,6 +5,12 @@ from enum import Enum
 def sqr(x):
     return x * x
 
+def inRange(x, lo, hi):
+    return (x >= lo) and (x <= hi)
+
+def inStrongRange(x, lo, hi):
+    return (x > lo) and (x < hi)
+
 class Joint:
     
     def __init__(self, position : m2.Vector2, stationary : bool = False):
@@ -49,17 +55,10 @@ class Joint:
     
     def __str__(self):
         return "Position = " + str(self.position) + "\tVelocity = " + str(self.velocity) + "\tForces = " + str(self.forces) + "\tInteria = " + str(self.interia) + "\tIsStationary = " + str(self.isStationary)
-    
-class ConnectionType(Enum):
-    
-    YOUNG = 0
-    SPRING = 1
-    LINEAR = YOUNG
-    SQUARE = SPRING
         
 class Connection:
     
-    def __init__(self, jointA : Joint, jointB : Joint, mass : float, maxCompression : float, compressionForceRate : float, maxStrech : float, strechForceRate : float, connectionType : ConnectionType = ConnectionType.SPRING):
+    def __init__(self, jointA : Joint, jointB : Joint, mass : float, maxCompression : float, compressionForceRate : float, maxStrech : float, strechForceRate : float):
         self.jointA : Joint = jointA
         self.jointB : Joint = jointB
         self.mass : float = mass
@@ -69,19 +68,14 @@ class Connection:
         self.strechForceRate : float = strechForceRate
         self.length : float = (self.jointA.position - self.jointB.position).length()
         self.broken = False
-        self.connectionType : ConnectionType = connectionType
         
     def getForce(self): # for jointA / Force jointB = - Force jointA
         v : m2.Vector2 = (self.jointA.position - self.jointB.position)
         currentLength : float = v.length()
-        if self.connectionType == ConnectionType.YOUNG:
-            f : float = sqr(currentLength - self.length) / 2
-        elif self.connectionType == ConnectionType.SPRING:
-            f : float = abs(currentLength - self.length)
         if currentLength < self.length:
-            return v.normal() * (-self.compressionForceRate * f)
+            return v.normal() * (-self.compressionForceRate * (currentLength - self.length))
         if currentLength > self.length:
-            return v.normal() * (-self.strechForceRate * f)
+            return v.normal() * (-self.strechForceRate * (currentLength - self.length))
         return m2.Vector2()
         
     def addForces(self, gravity : m2.Vector2 = m2.Vector2()):
@@ -110,13 +104,13 @@ class Connection:
         currentLength : float = v.length()
         
         if self.length == 0:
-            return 0
+            return 0.0
         
-        if currentLength > self.length:
-            return abs((currentLength-self.length) / self.length / (self.maxCompression-1))
+        if currentLength > self.length and inStrongRange(self.maxCompression, 0, 1):
+            return min(1.0, abs((currentLength-self.length) / self.length / (self.maxCompression-1)))
               
-        if currentLength < self.length:
-            return abs((currentLength-self.length) / self.length / (self.maxStrech-1))
+        elif currentLength < self.length and self.maxStrech > 1:
+            return min(1.0, abs((currentLength-self.length) / self.length / (self.maxStrech-1)))
             
         return 0
                 
@@ -130,6 +124,18 @@ class Connection:
                 self.broken = True
         return self.broken
         
+    def copy(self):
+        c = Connection(self.jointA, self.jointB, self.mass, self.maxCompression, self.compressionForceRate, self.maxStrech, self.strechForceRate)
+        c.broken = self.broken
+        return c
+        
+    def breakToTwo(self, where : float = 0.5):
+        j1 = Joint((self.jointA.position*where + self.jointB.position*(1-where)))
+        j1.velocity = self.jointA.velocity*where + self.jointB.velocity*(1-where)
+        j2 = j1.copy()
+        c1 = Connection(self.jointA, j1, self.mass*where, self.maxCompression, self.compressionForceRate, self.maxStrech, self.strechForceRate)
+        c2 = Connection(j2, self.jointB, self.mass*(1.0-where), self.maxCompression, self.compressionForceRate, self.maxStrech, self.strechForceRate)
+        return (j1, j2, c1, c2)
         
         
 class Bridge:
@@ -138,8 +144,90 @@ class Bridge:
         self.points = []
         self.connections = []
     
+    def copy(self):
+        b = Bridge()
+        i : int = 0
+        for p in self.points:
+            p.indexOnBridge = i
+            b.points.append(p.copy())
+            i += 1
+            
+        for connection in self.connections:
+            c = connection.copy()
+            c.jointA = b.points[c.jointA.indexOnBridge]
+            c.jointB = b.points[c.jointB.indexOnBridge]
+            b.connections.append(c)
+            
+        return b   
     
-def simulateTimeStep(bridge, timeStep : float = 1e-6, gravity : m2.Vector2 = m2.Vector2(0, -9.81), resistance : float = 1e-3, tol : float = 1e-12):
+    def getModelForRender(self, size : (int, int) = None, bounds : float = 1.3): # returns a touple of two lists of touples: with lines: (x1, y1, x2, y2, strain), with joints: (x, y, isStationary)
+        lines = []
+        points = []
+        i : int = 0
+        for p in self.points:
+            p.indexOnBridge = i
+            i += 1
+            
+        if i == 0:
+            return result
+        
+        epsilon : float = +1e-38;
+                    
+        rx : float = 0.0
+        ry : float = 0.0
+        k : float = 1.0
+        
+        if size != None:
+            maxX : int = 0
+            maxY : int = 0
+            minX : int = 0
+            minY : int = 0
+            for p in self.points:
+                if p.isStationary:
+                    v = p.position
+                    if v.x > maxX:
+                        maxX = v.x
+                    elif v.x < minX:
+                        minX = v.x
+                    if v.y > maxY:
+                        maxY = v.y
+                    elif v.y < minY:
+                        minY = v.y
+                    
+            k = min(size[0]/float(maxX-minX+epsilon), size[1]/float(maxY-minY+epsilon)) / bounds
+            rx = float(-minX) - float(maxX-minX)/2 + size[0]/k/2
+            ry = float(-minY) - float(maxY-minY)/2 + size[1]/k/2
+            
+        for connection in self.connections:
+            if not connection.broken:
+                lines.append(((connection.jointA.position.x+rx)*k, (connection.jointA.position.y+ry)*k, (connection.jointB.position.x+rx)*k, (connection.jointB.position.y+ry)*k, connection.getStrain()))
+            
+        for point in self.points:
+            points.append(((point.position.x+rx)*k, (point.position.y+ry)*k, point.isStationary))
+        
+        return (lines, points)
+        
+    
+    def render(self, fileName : str, width : int = 640, height : int = 480, bounds : float = 1.3):
+        
+        from PIL import Image, ImageDraw
+        
+        image = Image.new("RGB", (width, height), "white")
+        draw = ImageDraw.Draw(image)
+            
+        model = self.getModelForRender((width, height), bounds)
+        for line in model[0]:
+            draw.line([(line[0], height-line[1]-1), (line[2], height-line[3]-1)], width=5, fill=(int(255*line[4]) + int(255-255*line[4])*256), joint="curve")
+            
+        colors = {True: "purple", False: "red"}
+        
+        for point in model[1]:
+            draw.ellipse((point[0]-5, height-point[1]-5-1, point[0]+5, height-point[1]+5-1), outline=colors[point[2]], width=3, fill=None)
+                    
+        image.save(fileName)
+    
+    
+def simulateTimeStep(bridge, timeStep : float = 1e-6, gravity : m2.Vector2 = m2.Vector2(0, -9.81), resistance : float = 1e-3, tol : float = 1e-12, realBrakes : bool = False, toleranceCountDependent : bool = False, safeBreaking : bool = False):
    
     copyJoints = []
     orginalJoints = []
@@ -149,7 +237,7 @@ def simulateTimeStep(bridge, timeStep : float = 1e-6, gravity : m2.Vector2 = m2.
     delta = tol + 1
     it = 0
     
-    while delta > tol:
+    while delta > tol * {True: len(bridge.points)*len(bridge.connections), False: 1.0}[toleranceCountDependent]:
         
         if it > 0:
             for i in range(len(orginalJoints)):
@@ -179,12 +267,28 @@ def simulateTimeStep(bridge, timeStep : float = 1e-6, gravity : m2.Vector2 = m2.
         delta = 0
         for i in range(len(copyJoints)):
             delta += bridge.points[i].calcDelta(copyJoints[i])
-        
+            
         copyJoints.clear()
     
     orginalJoints.clear()
     
+    additions = []
     for connection in bridge.connections:
-        connection.checkBreaking()
+        if (not connection.broken) and connection.checkBreaking() and realBrakes:
+            additions.append(connection.breakToTwo())
+            
+    for v in additions:
+        bridge.points.append(v[0])
+        bridge.points.append(v[1])
+        if safeBreaking:
+            v[2].maxCompression /= 2
+            v[2].maxStrech *= 2
+            v[3].maxCompression /= 2
+            v[3].maxStrech *= 2
+        bridge.connections.append(v[2])
+        bridge.connections.append(v[3])
+        
+    additions.clear()
+            
     
     return timeStep * 2 
--- tests/test_tbsymulator/test_mechanics.py
+++ tests/test_tbsymulator/test_mechanics.py
@@ -1,20 +1,12 @@
 import tbsymulator.mechanics as mechanics
 import matplotlib.pyplot as plt
+from PIL import Image, ImageDraw
 
 def test_sqr():
     assert mechanics.sqr(2) == 4
     assert mechanics.sqr(-2) == 4
     assert mechanics.sqr(3) == 9
     assert mechanics.sqr(-3) == 9
-    
-def test_ConnectionType():
-    assert mechanics.ConnectionType.YOUNG == mechanics.ConnectionType.LINEAR
-    assert mechanics.ConnectionType.SPRING == mechanics.ConnectionType.SQUARE
-    assert isinstance(mechanics.ConnectionType.YOUNG, mechanics.ConnectionType)
-    assert isinstance(mechanics.ConnectionType.LINEAR, mechanics.ConnectionType)
-    assert isinstance(mechanics.ConnectionType.SQUARE, mechanics.ConnectionType)
-    assert isinstance(mechanics.ConnectionType.SPRING, mechanics.ConnectionType)
-    
     
 def createSampleBridge():
     bridge = mechanics.Bridge()
@@ -26,12 +18,12 @@ def createSampleBridge():
     bridge.points.append(mechanics.Joint([8.0, 2.0]))
     bridge.points.append(mechanics.Joint([5.0, 2.0]))
     bridge.points.append(mechanics.Joint([2.0, 2.0]))
-    bridge.points.append(mechanics.Joint([5.0, 5.0]))
+    #bridge.points.append(mechanics.Joint([5.0, 5.0]))
     
     defMaxCompression = 0.9
     defMaxStrech = 1.1
-    defCompressionForceRate = 1e3
-    defStrechForceRate = 1e3
+    defCompressionForceRate = 1e4
+    defStrechForceRate = 1e4
     defMass = 20
     
     # podłoże
@@ -47,12 +39,14 @@ def createSampleBridge():
     bridge.connections.append(mechanics.Connection(bridge.points[2], bridge.points[4], defMass, defMaxCompression, defCompressionForceRate, defMaxStrech, defStrechForceRate))
     bridge.connections.append(mechanics.Connection(bridge.points[3], bridge.points[4], defMass, defMaxCompression, defCompressionForceRate, defMaxStrech, defStrechForceRate))
     
-    # liny
-    bridge.connections.append(mechanics.Connection(bridge.points[6], bridge.points[5], 1, 0, 0, 1.05*defMaxStrech, 1.2*defStrechForceRate))
-    bridge.connections.append(mechanics.Connection(bridge.points[5], bridge.points[4], 1, 0, 0, 1.05*defMaxStrech, 1.2*defStrechForceRate))
+    # górne
+    #bridge.connections.append(mechanics.Connection(bridge.points[6], bridge.points[5], 1, 0, 0, 1.05*defMaxStrech, 1.2*defStrechForceRate))
+    #bridge.connections.append(mechanics.Connection(bridge.points[5], bridge.points[4], 1, 0, 0, 1.05*defMaxStrech, 1.2*defStrechForceRate))
+    bridge.connections.append(mechanics.Connection(bridge.points[6], bridge.points[5], defMass, defMaxCompression, defCompressionForceRate, defMaxStrech, defStrechForceRate))
+    bridge.connections.append(mechanics.Connection(bridge.points[5], bridge.points[4], defMass, defMaxCompression, defCompressionForceRate, defMaxStrech, defStrechForceRate))
     
     # huśtawka
-    bridge.connections.append(mechanics.Connection(bridge.points[6], bridge.points[7], defMass, defMaxCompression, defCompressionForceRate, defMaxStrech, defStrechForceRate))
+    #bridge.connections.append(mechanics.Connection(bridge.points[6], bridge.points[7], defMass, defMaxCompression, defCompressionForceRate, defMaxStrech, defStrechForceRate))
     
     return bridge
     
@@ -74,11 +68,73 @@ def createSampleBridgePendulum():
     
     return bridge
     
-    
+
 def test_createSampleBridge():
     assert isinstance(createSampleBridge(), mechanics.Bridge)
     assert isinstance(createSampleBridgePendulum(), mechanics.Bridge)
     
+def test_CopyBridge():
+    
+    b1 = createSampleBridgePendulum()
+    b2 = b1.copy()
+    
+    b1.connections[0].jointA.position.x = 10
+    assert b2.connections[0].jointA.position.x != 10
+    
+    
+def test_RenderBridge():
+    bridge = createSampleBridge()
+    model = bridge.getModelForRender((640, 480))[0]
+    
+    assert isinstance(model, list)    
+    
+    mechanics.simulateTimeStep(bridge, 1e-6, tol = 1e-3, resistance = 1e-3, realBrakes = True)
+    
+    image = Image.new("RGB", (640, 480), "white")
+    draw = ImageDraw.Draw(image)
+        
+    for line in model:
+        assert line[0] >= 0
+        assert line[1] >= 0
+        assert line[2] >= 0
+        assert line[3] >= 0
+        assert line[0] < 640
+        assert line[1] < 480
+        assert line[2] < 640
+        assert line[3] < 480
+        draw.line([(line[0], 480-line[1]), (line[2], 480-line[3])], width=5, fill=(int(255*line[4]) + int(255-255*line[4])*256), joint="curve")
+    
+    image.save("out1.png")    
+    bridge.render("out2.png")
+        
+    
+def test_RenderBridge2():
+    skip = False
+    if skip:
+        return
+    
+    bridge = createSampleBridge()
+    endTime = 15
+    fps = 30
+    frameCount = 0
+    prevFrame = 0.0
+    time = 0.0
+    deltaTime = 1e-6
+    
+    while time < endTime:
+        deltaTime = mechanics.simulateTimeStep(bridge, deltaTime, tol = 1e-3, resistance = 0.5, realBrakes = True, toleranceCountDependent = True)
+        time += deltaTime
+        #deltaTime = min(1.0/fps/2, deltaTime)
+        #print(time)
+        
+        if time >= prevFrame + 1.0/fps:
+            bridge.render("frame" + str(frameCount) + ".png")
+            print("frame: " + str(frameCount))
+            frameCount += 1
+            prevFrame = time
+    
+        #print(time, deltaTime, bridge.connections[len(bridge.connections)-1].getStrain(), bridge.points[len(bridge.points)-1].position.x, bridge.points[len(bridge.points)-1].position.y, bridge.points[len(bridge.points)-1].velocity.x, bridge.points[len(bridge.points)-1].velocity.y, bridge.points[len(bridge.points)-1].velocity.length())
+               
     
 def test_simulation():
     bridge = createSampleBridgePendulum()
@@ -88,7 +144,7 @@ def test_simulation():
     
     it = 0
     
-    endTime = 15 # 20.55s
+    endTime = 5 #15 # 20.55s
     
     timeValues = []
     XValues = []
@@ -98,8 +154,8 @@ def test_simulation():
     VValues = []
     strainValues = []
     
-    StdOut = True
-    Plots = True
+    StdOut = False
+    Plots = False
     
     PlotInterval = 0.01
     
@@ -110,7 +166,7 @@ def test_simulation():
         print("time", "dt", "strain", "X", "Y", "Vx", "Vy", "V")
     
     while time < endTime:
-        deltaTime = mechanics.simulateTimeStep(bridge, deltaTime, tol = 1e-3, resistance = 1e-1)
+        deltaTime = mechanics.simulateTimeStep(bridge, deltaTime, tol = 1e-3, resistance = 1e-1, realBrakes = True)
         time += deltaTime
         it += 1
         
