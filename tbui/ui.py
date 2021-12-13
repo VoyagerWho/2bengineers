e34c918618e80eda779c4f467c859a83080a9894
test <j.b.baczewska@gmail.com>
2021-12-13 21:29:46 +0100

    UI functionality demo

--- tbui/ui.py
+++ tbui/ui.py
@@ -1,12 +1,14 @@
 from tbutils.builder import Builder
 import tbutils.math2d as m2d
 from vpython import *
+import tbneuralnetwork.nueralnetworkfunctions as nnf
 
 canvasWidth = 1600
 canvasHeight = 480
 pickingPoints = True
 numberOfStaticPoints = 0
 staticPoints = []
+generatedCurves = []
 
 def stopPickingPoints(b):
     global pickingPoints
@@ -29,6 +31,15 @@ def deletePoints(b):
 
     print("after delete: ")
     print(staticPoints)
+
+def deleteCurves():
+    global generatedCurves
+    numberOfCurves = len(generatedCurves)
+    for i in range(0, numberOfCurves, 1):
+        generatedCurves[0].visible = False
+        del generatedCurves[0]
+    # print("after delete: ")
+    # print(generatedCurves)
 
 def ui():
     global pickingPoints, staticPoints
@@ -83,6 +94,23 @@ def ui():
             print(s.pos.x)
             print(evt.pos.x)
 
+    def pickLines(bridge):
+        l = []
+        for connection in bridge.connections:
+            if not connection.broken:
+                l.append((connection.jointA.position.x, connection.jointA.position.y,
+                          connection.jointB.position.x, connection.jointB.position.y,
+                          connection.getStrain()))
+        return l
+
+    def showBridge(bridge, l):
+        for ln in l:
+            list_of_points = []
+            list_of_points.append(vector(ln[0], ln[1], 0))
+            list_of_points.append(vector(ln[2], ln[3], 0))
+            c = curve(pos=list_of_points, color=color.red)
+            generatedCurves.append(c)
+
     scene.bind('mousedown', grab)
     scene.bind('mousemove', move)
     scene.bind('mouseup', drop)
@@ -125,33 +153,86 @@ def ui():
         bridge = Builder.buildInitial(materials, point1, point2, numberOfExtraStaticPoints, addedStaticPoints)
         print("after2")
 
-    l = []
-    for connection in bridge.connections:
-        if not connection.broken:
-            l.append((connection.jointA.position.x, connection.jointA.position.y,
-                      connection.jointB.position.x, connection.jointB.position.y,
-                      connection.getStrain()))
-    print("Result: ")
-    print(l)
-    print(len(l))
+    l = pickLines(bridge)
 
-    p = list(bridge.points)
-
-    # print(scene.camera.pos)
-    # print(scene.camera.axis)
-    # print(scene.center)
+    # p = list(bridge.points)
 
     scene.center = vector(canvasWidth / 2.0, canvasHeight / 2.0, 0)
     scene.camera.axis.z = 900
+    showBridge(bridge, l)
 
-    # print(scene.camera.pos)
-    # print(scene.camera.axis)
-    # print(scene.center)
+    # below - functionality demo
+    sleep(2)
+    print("changeConnectionMaterial")
+    nnf.changeConnectionMaterial(bridge, 10, 0.1)
+    l = pickLines(bridge)
+    deleteCurves()
+    showBridge(bridge, l)
 
-    for ln in l:
-        list_of_points = []
-        list_of_points.append(vector(ln[0], ln[1], 0))
-        list_of_points.append(vector(ln[2], ln[3], 0))
-        curve(pos=list_of_points, color=color.red)
+    sleep(1)
+    print("removeConnection")
+    nnf.removeConnection(bridge, 7, 0.3)
+    l = pickLines(bridge)
+    deleteCurves()
+    showBridge(bridge, l)
+
+    sleep(1)
+    print("removeJoint")
+    nnf.removeJoint(bridge, 8, 0.3, 0.6)
+    l = pickLines(bridge)
+    deleteCurves()
+    showBridge(bridge, l)
+
+    sleep(1)
+    print("removeJoint")
+    nnf.removeJoint(bridge, 4, 0.3, 0.6)
+    l = pickLines(bridge)
+    deleteCurves()
+    showBridge(bridge, l)
+
+    sleep(1)
+    print("addJoint")
+    nnf.addJoint(bridge, 10, 0.5, 0.1)
+    l = pickLines(bridge)
+    deleteCurves()
+    showBridge(bridge, l)
+
+    sleep(1)
+    print("addJoint")
+    nnf.addJoint(bridge, 12, 0.5, 0.1)
+    l = pickLines(bridge)
+    deleteCurves()
+    showBridge(bridge, l)
+
+    sleep(1)
+    print("moveJoint")
+    nnf.moveJoint(bridge, 6, 0.3, 0.6)
+    l = pickLines(bridge)
+    deleteCurves()
+    showBridge(bridge, l)
+
+    sleep(1)
+    print("moveJoint")
+    nnf.moveJoint(bridge, 7, 0.3, 0.6)
+    l = pickLines(bridge)
+    deleteCurves()
+    showBridge(bridge, l)
+
+    sleep(1)
+    print("moveJoint")
+    nnf.moveJoint(bridge, 10, 0.3, 0.6)
+    l = pickLines(bridge)
+    deleteCurves()
+    showBridge(bridge, l)
+
+    sleep(1)
+    print("addConnection")
+    nnf.addConnection(bridge, 3, 0.9, 0.3)
+    nnf.addConnection(bridge, 2, 0.9, 0.3)
+    nnf.addConnection(bridge, 1, 0.9, 0.3)
+    nnf.addConnection(bridge, 4, 0.9, 0.3)
+    l = pickLines(bridge)
+    deleteCurves()
+    showBridge(bridge, l)
 
     print("END")
