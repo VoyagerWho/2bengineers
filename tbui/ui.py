a8d53be5d6d7fe1ef5f0f7729f298bede04915e5
test <j.b.baczewska@gmail.com>
2022-01-09 15:13:26 +0100

    Code cleanup 2: part of code extracted to separate functions

--- tbui/ui.py
+++ tbui/ui.py
@@ -12,19 +12,20 @@ import os
 canvasWidth = 1600
 canvasHeight = 480
 picking_points = True
-numberOfStaticPoints = 0
 staticPoints = []
-# for double bridge
+
+static_load = 2000
+show_natural = True
+run_showing_bridge_next_steps = True
+wait_for_click = False
+
+#
 generatedCurves1 = []
 generatedCurves2 = []
 generatedRoad = []
 generatedPoints = []
 generatedTerrain = []
 generatedExtraPoles = []
-static_load = 2000
-show_natural = True
-run_showing_bridge_next_steps = True
-wait_for_click = False
 
 # colors
 color_background = vec(0.9, 0.9, 1)
@@ -34,6 +35,16 @@ color_wood = vec(0.6, 0.25, 0.02)
 color_green = vec(0.3, 0.9, 0.3)
 color_blue = vec(0.6, 0.6, 1)
 color_concrete = vec(0.5, 0.5, 0.5)
+
+hight_of_terrain = 400
+hight_of_cliff = 200
+
+posX = 0
+posY = 0
+length_x = 0
+length_y = 0
+position_x = 0
+position_y = 0
 
 
 class thread_with_exception(threading.Thread):
@@ -70,7 +81,25 @@ class thread_with_exception(threading.Thread):
             print('Exception raise failure')
 
 
+def pick_lines(bridge):
+    lines = []
+    for connection in bridge.connections:
+        if not connection.broken:
+            lines.append((connection.jointA.position.x, connection.jointA.position.y,
+                          connection.jointB.position.x, connection.jointB.position.y,
+                          connection.getStrain(), connection.material))
+    return lines
+
+
+def pick_points(bridge):
+    points = []
+    for point in bridge.points:
+        points.append(point)
+    return points
+
+
 def stop_picking_points(b):
+    global staticPoints
     if len(staticPoints) >= 2:
         global picking_points
         picking_points = False
@@ -138,12 +167,6 @@ def delete_extra_poles():
         del generatedExtraPoles[0]
 
 
-def async_crazy_stuff():
-    chamber2 = ai.BridgeEvolution((os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'tbneuralnetwork'))))
-    chamber2.load()
-    chamber2.upgrade("", 1)
-
-
 def add_static_load(wi):
     global static_load
 
@@ -166,8 +189,126 @@ def clear_my_scene():
     delete_extra_poles()
 
 
+def generate_terrain():
+    global staticPoints, hight_of_terrain, hight_of_cliff, posX, posY, length_x, length_y, position_x, position_y
+    left_point = None
+    rigth_point = None
+    depth = 1000
+
+    if staticPoints[0].pos.x < staticPoints[1].pos.x:
+        left_point = staticPoints[0]
+        rigth_point = staticPoints[1]
+    else:
+        left_point = staticPoints[1]
+        rigth_point = staticPoints[0]
+
+    length_x_not_abs = staticPoints[1].pos.x - staticPoints[0].pos.x
+    length_y_not_abs = staticPoints[1].pos.y - staticPoints[0].pos.y
+    length_x = abs(length_x_not_abs)
+    length_y = abs(length_y_not_abs)
+
+    grass_left_position_x = left_point.pos.x - 500
+    grass_left_position_y = left_point.pos.y
+    grass_right_position_x = rigth_point.pos.x + 500
+    grass_right_position_y = rigth_point.pos.y
+
+    if staticPoints[0].pos.x < staticPoints[1].pos.x:
+        posX = staticPoints[0].pos.x
+    else:
+        posX = staticPoints[1].pos.x
+
+    if staticPoints[0].pos.y < staticPoints[1].pos.y:
+        posY = staticPoints[0].pos.y
+    else:
+        posY = staticPoints[1].pos.y
+
+    position_x = length_x / 2 + posX
+    position_y = length_y / 2 + posY
+
+    terrain_shape = shapes.points(
+        pos=[[0, 0], [0, -length_y - 200], [-length_x, -length_y - 200], [-length_x, -length_y],
+             [-length_x - 1000, -length_y],
+             [-length_x - 1000, -length_y - hight_of_terrain], [1000, -length_y - hight_of_terrain], [1000, 0]])
+
+    terrain_path = [vec(position_x, position_y, 0),
+                    vec(position_x, position_y, depth)]
+
+    grass1_front = box(pos=vec(grass_left_position_x, grass_left_position_y, 275),
+                       length=1000,
+                       height=1, width=450, color=color_green)
+    grass2_front = box(pos=vec(grass_right_position_x, grass_right_position_y, 275),
+                       length=1000,
+                       height=1, width=450, color=color_green)
+
+    grass1_back = box(pos=vec(grass_left_position_x, grass_left_position_y, -275),
+                      length=1000,
+                      height=1, width=450, color=color_green)
+    grass2_back = box(pos=vec(grass_right_position_x, grass_right_position_y, -275),
+                      length=1000,
+                      height=1, width=450, color=color_green)
+
+    river = box(pos=vec(position_x, posY - hight_of_cliff, 0),
+                length=length_x,
+                height=1, width=depth, color=color_blue)
+
+    road_terrain1 = box(pos=vec(grass_left_position_x, grass_left_position_y, 0),
+                        length=1000,
+                        height=1, width=100, color=color_asphalt)
+
+    road_terrain2 = box(pos=vec(grass_right_position_x, grass_right_position_y, 0),
+                        length=1000,
+                        height=1, width=100, color=color_asphalt)
+
+    terrain = extrusion(path=terrain_path, shape=terrain_shape, visible=False)
+
+    if (staticPoints[0].pos.x < staticPoints[1].pos.x) and (staticPoints[0].pos.y < staticPoints[1].pos.y):
+        terrain.rotate(angle=pi, axis=vec(0, position_y, 0))
+    elif (staticPoints[1].pos.x < staticPoints[0].pos.x) and (staticPoints[1].pos.y < staticPoints[0].pos.y):
+        terrain.rotate(angle=pi, axis=vec(0, position_y, 0))
+
+    terrain.pos = vec(position_x, position_y - hight_of_terrain / 2, 0)
+    terrain.color = vec(0.5, 0.2, 0.02)
+
+    generatedTerrain.append(terrain)
+    generatedTerrain.append(grass1_front)
+    generatedTerrain.append(grass2_front)
+    generatedTerrain.append(grass1_back)
+    generatedTerrain.append(grass2_back)
+    generatedTerrain.append(river)
+    generatedTerrain.append(road_terrain1)
+    generatedTerrain.append(road_terrain2)
+    terrain.visible = True
+
+
+def generate_extra_poles():
+    global hight_of_terrain, hight_of_cliff, posX, posY, length_x, length_y
+    over_bridge_level = 200
+    picked_points = pick_points(ai.BridgeEvolution.bridge)
+    for picked_point in picked_points:
+        if picked_point.isStationary:
+            connected = ai.BridgeEvolution.bridge.getConnectedToJoint(picked_point)
+            if len(connected) > 0:
+                bridge_level_from_bottom = length_y / length_x * (picked_point.position[0] - posX)
+                final_high = (over_bridge_level + bridge_level_from_bottom + hight_of_terrain)
+                static_pole1 = box(
+                    pos=vec(picked_point.position[0], final_high / 2 - hight_of_terrain + posY, 55),
+                    length=30,
+                    height=final_high, width=10, color=color_concrete)
+                static_pole2 = box(
+                    pos=vec(picked_point.position[0], final_high / 2 - hight_of_terrain + posY, -55),
+                    length=30,
+                    height=final_high, width=10, color=color_concrete)
+                static_bar = box(
+                    pos=vec(picked_point.position[0], final_high - hight_of_terrain + posY - 5, 0),
+                    length=30,
+                    height=10, width=100, color=color_concrete)
+
+                generatedExtraPoles.append(static_pole1)
+                generatedExtraPoles.append(static_pole2)
+                generatedExtraPoles.append(static_bar)
+
+
 def ui():
-    # t1 = threading.Thread(target=async_crazy_stuff)
     first_start = True
     t1 = None
     global picking_points, staticPoints, wait_for_click
@@ -177,7 +318,6 @@ def ui():
     def change_way_to_present_bridge(b):
         global show_natural
         if b.checked:
-            print("CHANGE WAY TO PRESENT BRIDGE!")
             show_natural = b.natural
         for i in range(len(radio_buttons)):
             if i != b.i:
@@ -191,7 +331,6 @@ def ui():
 
         print("RESTART_THIS")
         if t1 is not None and t1.is_alive():
-            print("INSIDE")
             t1.raise_exception()
             t1.join()
 
@@ -245,21 +384,6 @@ def ui():
             s.color = color.cyan
             drag = False
             s.pos = scene.mouse.pos
-
-    def pick_lines(bridge):
-        lines = []
-        for connection in bridge.connections:
-            if not connection.broken:
-                lines.append((connection.jointA.position.x, connection.jointA.position.y,
-                              connection.jointB.position.x, connection.jointB.position.y,
-                              connection.getStrain(), connection.material))
-        return lines
-
-    def pick_points(bridge):
-        points = []
-        for point in bridge.points:
-            points.append(point)
-        return points
 
     def show_bridge(bridge, position_z, version):
         picked_lines = pick_lines(bridge)
@@ -339,14 +463,15 @@ def ui():
     scene.bind('mouseup', drop)
 
     def start_everything():
+        global run_showing_bridge_next_steps, wait_for_click
+        nonlocal first_start, t1
+
         clear_my_scene()
         scene.axis = vector(0, 0, -1)
         scene.center = vector(canvasWidth / 2.0, canvasHeight / 2.0, 0)
-        nonlocal first_start
+
         first_start = False
         print("START_EVERYTHING")
-        global run_showing_bridge_next_steps, wait_for_click
-        nonlocal t1
 
         t1 = thread_with_exception('Thread 1')
         bridge_obj = None
@@ -357,7 +482,6 @@ def ui():
             wait_for_click = False
             rate(10)
 
-        print(staticPoints)
         if len(staticPoints) >= 1:
             point1 = m2d.Vector2(staticPoints[0].pos.x, staticPoints[0].pos.y)
         if len(staticPoints) >= 2:
@@ -373,115 +497,18 @@ def ui():
         if len(staticPoints) == 2:
             bridge_obj = Builder.buildInitial(materials, point1, point2)
             bridge_obj.roadStrains = static_load
-            bridge_obj.render("renderCheck.png")
         elif len(staticPoints) > 2:
             bridge_obj = Builder.buildInitial(materials, point1, point2, number_of_extra_static_points,
                                               added_static_points)
             bridge_obj.roadStrains = static_load
-            bridge_obj.render("renderCheck.png")
 
-        print("%f %f %f %f %f %f", staticPoints[0].pos.x, staticPoints[0].pos.y, staticPoints[1].pos.x,
-              staticPoints[1].pos.y)
-        print(point1)
-
-        left_point = 0
-        rigth_point = 0
-        if staticPoints[0].pos.x < staticPoints[1].pos.x:
-            left_point = staticPoints[0]
-            rigth_point = staticPoints[1]
-        else:
-            left_point = staticPoints[1]
-            rigth_point = staticPoints[0]
-
-        length_x_not_abs = staticPoints[1].pos.x - staticPoints[0].pos.x
-        length_y_not_abs = staticPoints[1].pos.y - staticPoints[0].pos.y
-        length_x = abs(length_x_not_abs)
-        length_y = abs(length_y_not_abs)
-
-        posX = 0
-        posY = 0
-        grass_left_position_x = left_point.pos.x - 500
-        grass_left_position_y = left_point.pos.y
-        grass_right_position_x = rigth_point.pos.x + 500
-        grass_right_position_y = rigth_point.pos.y
-
-        if staticPoints[0].pos.x < staticPoints[1].pos.x:
-            posX = staticPoints[0].pos.x
-        else:
-            posX = staticPoints[1].pos.x
-
-        if staticPoints[0].pos.y < staticPoints[1].pos.y:
-            posY = staticPoints[0].pos.y
-        else:
-            posY = staticPoints[1].pos.y
-
-        position_x = length_x / 2 + posX
-        position_y = length_y / 2 + posY
-
-        hight_of_terrain = 400
-        hight_of_cliff = 200
-        depth = 1000
-        terrain_shape = shapes.points(
-            pos=[[0, 0], [0, -length_y - 200], [-length_x, -length_y - 200], [-length_x, -length_y],
-                 [-length_x - 1000, -length_y],
-                 [-length_x - 1000, -length_y - hight_of_terrain], [1000, -length_y - hight_of_terrain], [1000, 0]])
-
-        terrain_path = [vec(position_x, position_y, 0),
-                        vec(position_x, position_y, depth)]
-
-        grass1_front = box(pos=vec(grass_left_position_x, grass_left_position_y, 275),
-                           length=1000,
-                           height=1, width=450, color=color_green)
-        grass2_front = box(pos=vec(grass_right_position_x, grass_right_position_y, 275),
-                           length=1000,
-                           height=1, width=450, color=color_green)
-
-        grass1_back = box(pos=vec(grass_left_position_x, grass_left_position_y, -275),
-                          length=1000,
-                          height=1, width=450, color=color_green)
-        grass2_back = box(pos=vec(grass_right_position_x, grass_right_position_y, -275),
-                          length=1000,
-                          height=1, width=450, color=color_green)
-
-        river = box(pos=vec(position_x, posY - hight_of_cliff, 0),
-                    length=length_x,
-                    height=1, width=depth, color=color_blue)
-
-        road_terrain1 = box(pos=vec(grass_left_position_x, grass_left_position_y, 0),
-                            length=1000,
-                            height=1, width=100, color=color_asphalt)
-
-        road_terrain2 = box(pos=vec(grass_right_position_x, grass_right_position_y, 0),
-                            length=1000,
-                            height=1, width=100, color=color_asphalt)
+        generate_terrain()
 
         scene.center = vector(canvasWidth / 2.0, canvasHeight / 2.0, 0)
         scene.camera.axis.z = 900
         show_double_bridge(bridge_obj)
         scene.autoscale = False
-        terrain = extrusion(path=terrain_path, shape=terrain_shape, visible=False)
-        generatedTerrain.append(terrain)
-        generatedTerrain.append(grass1_front)
-        generatedTerrain.append(grass2_front)
-        generatedTerrain.append(grass1_back)
-        generatedTerrain.append(grass2_back)
-        generatedTerrain.append(river)
-        generatedTerrain.append(road_terrain1)
-        generatedTerrain.append(road_terrain2)
-        # terrain.rotate(angle=pi, axis=vec(position_x, 0, 0))
-        print("POINTS: ")
-        print(staticPoints[0].pos)
-        print(staticPoints[1].pos)
 
-        if (staticPoints[0].pos.x < staticPoints[1].pos.x) and (staticPoints[0].pos.y < staticPoints[1].pos.y):
-            terrain.rotate(angle=pi, axis=vec(0, position_y, 0))
-        elif (staticPoints[1].pos.x < staticPoints[0].pos.x) and (staticPoints[1].pos.y < staticPoints[0].pos.y):
-            terrain.rotate(angle=pi, axis=vec(0, position_y, 0))
-
-        terrain.pos = vec(position_x, position_y - hight_of_terrain / 2, 0)
-        terrain.color = vec(0.5, 0.2, 0.02)
-
-        terrain.visible = True
         ai.BridgeEvolution.bridge = bridge_obj
         ai.BridgeEvolution.upgrade_still_running = True
 
@@ -489,51 +516,27 @@ def ui():
         wtext_status.text = "\n\nStatus: simulation in progres."
         scene.axis = vector(-0.449187, -0.572867, -0.685605)
         scene.center = vector(position_x, position_y, 0)
-        over_bridge_level = 200
+
         run_showing_bridge_next_steps = True
         while run_showing_bridge_next_steps:
             wtext_progress.text = "\n\nProgress: already done %d simulations" % mechanics.executedSimulation
             show_double_bridge(ai.BridgeEvolution.bridge)
             delete_extra_poles()
             if number_of_extra_static_points > 0:
-                picked_points = pick_points(ai.BridgeEvolution.bridge)
-                for picked_point in picked_points:
-                    if picked_point.isStationary:
-                        connected = ai.BridgeEvolution.bridge.getConnectedToJoint(picked_point)
-                        print(len(connected))
-                        if len(connected) > 0:
-                            bridge_level_from_bottom = length_y / length_x * (picked_point.position[0] - posX)
-                            final_high = (over_bridge_level + bridge_level_from_bottom + hight_of_terrain)
-                            static_pole1 = box(
-                                pos=vec(picked_point.position[0], final_high / 2 - hight_of_terrain + posY, 55),
-                                length=30,
-                                height=final_high, width=10, color=color_concrete)
-                            static_pole2 = box(
-                                pos=vec(picked_point.position[0], final_high / 2 - hight_of_terrain + posY, -55),
-                                length=30,
-                                height=final_high, width=10, color=color_concrete)
-                            static_bar = box(
-                                pos=vec(picked_point.position[0], final_high - hight_of_terrain + posY - 5, 0),
-                                length=30,
-                                height=10, width=100, color=color_concrete)
-
-                            generatedExtraPoles.append(static_pole1)
-                            generatedExtraPoles.append(static_pole2)
-                            generatedExtraPoles.append(static_bar)
-
+                generate_extra_poles()
             if ai.BridgeEvolution.upgrade_still_running:
                 wtext_status.text = "\n\nStatus: simulation in progres"
             elif mechanics.road_broke:
                 wtext_status.text = "\n\nStatus: simulation ended, bridge broken."
             else:
                 wtext_status.text = "\n\nStatus: bridge simulation completed successfully, bridge stable."
-            rate(2)
+            rate(1)
 
     if first_start:
         start_everything()
 
     while True:
         print(".", sep=" ")
-        rate(2)
+        rate(1)
 
     print("END")
